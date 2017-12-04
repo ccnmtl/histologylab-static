17c7ef4391b816ef0f16cde44c84e052debaeffb
Nick Buonincontri <nbuonin@gmail.com>
2017-12-04 17:49:21 -0500

    Configure Spider to crawl all internal pages

--- scrape/histology/histology/spiders/histology_spider.py
+++ scrape/histology/histology/spiders/histology_spider.py
@@ -1,8 +1,10 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 
 class HistologySpider(scrapy.Spider):
     name = "histology"
+    extractor = LinkExtractor(allow_domains='pathologylab.ccnmtl.columbia.edu')
 
     def start_requests(self):
         urls = [
@@ -12,5 +14,8 @@ class HistologySpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # enter what you want this thing to do in here
         print("The response is here: %s" % response.url)
+        links = self.extractor.extract_links(response)
+
+        for link in links:
+            yield response.follow(link)
