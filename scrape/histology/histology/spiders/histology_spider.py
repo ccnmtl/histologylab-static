a8bd517c0986ee73f06b2c83268d3a67c2cb499e
Nick Buonincontri <nbuonin@gmail.com>
2017-12-14 15:56:26 -0500

    Refactor scrape script
    This commit reworks the scrape commit to put the content in directories
    matching the existing site.  It also ensures that each file has a unique
    file name.

--- scrape/histology/histology/spiders/histology_spider.py
+++ scrape/histology/histology/spiders/histology_spider.py
@@ -24,14 +24,14 @@ def slugify(value):
 
 class HistologySpider(scrapy.Spider):
     name = "histology"
-    extractor = LinkExtractor(allow_domains='pathologylab.ccnmtl.columbia.edu')
+    extractor = LinkExtractor(allow_domains='histologylab.ccnmtl.columbia.edu')
     match_lab_topic = re.compile('/lab\d{2}/$')
     match_lab_activity = re.compile('/lab\d{2}/\w*.html$')
     match_hist_technique = re.compile('/histological_techniques/\w*.html$')
 
     def start_requests(self):
         urls = [
-            'http://pathologylab.ccnmtl.columbia.edu/'
+            'http://histologylab.ccnmtl.columbia.edu/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -51,19 +51,15 @@ class HistologySpider(scrapy.Spider):
             yield response.follow(link)
 
     def render_lab_topic(self, response):
-        title = slugify(response.css('.entrytitle::text')
-                        .extract_first()) + '.md'
-        path = 'content/lab_topic/'
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        filename = os.path.join(path, title)
-
-        lab_topic = response.url.split('/')[-1]
-        lab_topic_number = int(re.search(r'\d{2}', lab_topic).group(0))
-        with open(filename, 'w') as f:
+        # Filter for labXX
+        lab_topic = [t for t in response.url.split('/')
+                     if re.search(r'lab\d{2}', t)]
+        lab_topic_number = int(re.search(r'\d{2}', lab_topic[0]).group(0))
+        path = 'content/'
+        title = 'lab{:02d}'.format(lab_topic_number)
+        with open(self.get_file_name(path, title), 'w') as f:
             f.write('---')
-            f.write('title: {}\n'.format(title))
+            f.write('title: "lab{:02d}"\n'.format(lab_topic_number))
             f.write('date: {}\n'.format(datetime.date.today().isoformat()))
             f.write('type: lab_topic\n')
             f.write('lab_topic_number: {}\n'.format(lab_topic_number))
@@ -71,55 +67,57 @@ class HistologySpider(scrapy.Spider):
             f.write('---\n')
 
     def render_lab_activity(self, response):
-        title = slugify(response.css('.entrytitle::text')
-                        .extract_first()) + '.md'
-        path = 'content/lab_activity/'
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        filename = os.path.join(path, title)
+        title = response.css('.entrytitle::text').extract_first(default='index')
 
         lab_topic = response.url.split('/')[-2]
         lab_topic_number = int(re.search(r'\d{2}', lab_topic).group(0))
-        with open(filename, 'w') as f:
+
+        path = 'content/lab{:02d}/'.format(lab_topic_number)
+        with open(self.get_file_name(path, title), 'w') as f:
             f.write('---\n')
-            f.write('title: {}\n'.format(title))
+            f.write('title: "{}"\n'.format(title))
             f.write('date: {}\n'.format(datetime.date.today().isoformat()))
             f.write('type: lab_activity\n')
             f.write('lab_topic_number: {}\n'.format(lab_topic_number))
+            f.write('lab_topic_name: "lab{:02d}"\n'.format(lab_topic_number))
             f.write('weight: \n')
             f.write('---\n')
-            f.write(response.css('.entrybody').extract_first())
+            f.write(response.css('.entrybody').extract_first(default=''))
 
     def render_hist_technique(self, response):
-        title = slugify(response.css('.entrytitle::text')
-                        .extract_first()) + '.md'
+        title = response.css('.entrytitle::text').extract_first(default='index')
         path = 'content/histological_technique/'
-        if not os.path.exists(path):
-            os.makedirs(path)
 
-        filename = os.path.join(path, title)
-        with open(filename, 'w') as f:
+        with open(self.get_file_name(path, title), 'w') as f:
             f.write('---')
-            f.write('title: {}\n'.format(title))
+            f.write('title: "{}"\n'.format(title))
             f.write('date: {}\n'.format(datetime.date.today().isoformat()))
             f.write('type: histology_technique\n')
             f.write('weight: \n')
             f.write('---\n')
-            f.write(response.css('.entrybody').extract_first())
+            f.write(response.css('.entrybody').extract_first(default=''))
 
     def render_page(self, response):
-        title = slugify(response.css('.pagetitle::text')
-                        .extract_first()) + '.md'
+        title = response.css('.entrytitle::text').extract_first(default='index')
         path = 'content/'
-        if not os.path.exists(path):
-            os.makedirs(path)
 
-        filename = os.path.join(path, title)
-        with open(filename, 'w') as f:
+        with open(self.get_file_name(path, title), 'w') as f:
             f.write('---\n')
             f.write('title: {}\n'.format(title))
             f.write('date: {}\n'.format(datetime.date.today().isoformat()))
             f.write('type: page\n')
             f.write('---\n')
-            f.write(response.css('.entrybody').extract_first())
+            f.write(response.css('.entrybody')
+                    .extract_first(default=response.url))
+
+    def get_file_name(self, path, title):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        filename = os.path.join(path, slugify(title) + '.md')
+        i = 1
+        while os.path.exists(filename):
+            filename = os.path.join(path, slugify(title) + '_' + str(i) + '.md')
+            i += 1
+
+        return filename
