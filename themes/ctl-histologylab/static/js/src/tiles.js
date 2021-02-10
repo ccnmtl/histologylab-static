068baaf14a8a499015e0b6030e9a9067f5015933
ndittren <36773036+ndittren@users.noreply.github.com>
2021-02-10 13:15:10 -0500

    prototype slideviewer

--- themes/ctl-histologylab/static/js/src/tiles.js
+++ themes/ctl-histologylab/static/js/src/tiles.js
@@ -2,15 +2,12 @@
 /* eslint-env node */
 
 $(document).ready(function() {
-    console.log('leaflet', L);
     var map = L.map('map', {
         center: [0,0],
         zoom: 2
     });
 
     var slideId = $('#map').data('slide-id');
-    //var slide_url_format = 'http://localhost:8000/{z}/{y}/{x}.jpg';
-    // var slide_url_format = 'https://cuimc-it-path-test.s3.amazonaws.com/slide' + slideId + '/{z}/{y}/{x}.jpg';
     var slide_url_format = 'https://ctl-locustempus-local.s3.amazonaws.com/slide' + slideId + '/{z}/{y}/{x}.jpg';
 
     L.tileLayer(slide_url_format, {
