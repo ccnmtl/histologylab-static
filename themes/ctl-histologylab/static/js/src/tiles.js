b95522c87507710422d5dac922c568ea1bd7b94c
ndittren <36773036+ndittren@users.noreply.github.com>
2021-02-22 22:33:51 -0500

    Implemented minimap view

--- themes/ctl-histologylab/static/js/src/tiles.js
+++ themes/ctl-histologylab/static/js/src/tiles.js
@@ -8,10 +8,31 @@ $(document).ready(function() {
 
     var slideId = $('#map').data('slide-id');
     var slide_url_format = 'https://cuimc-it-path-test.s3.amazonaws.com/slide' + slideId + '/{z}/{y}/{x}.jpg';
+
+    //Sets bounds for dragging.
+    var southWest = L.latLng(-90, -200)
+    var northEast = L.latLng(90, 200);
+    var bounds = L.latLngBounds(southWest, northEast);
+
+    map.on('drag', function() {
+        map.panInsideBounds(bounds, { animate: false });
+    });
+
     L.tileLayer(slide_url_format, {
-        minZoom: 2,
+        minZoom: 1,
         maxZoom: 9,
         noWrap: true
     }).addTo(map);
+
+    var smallLayer = L.tileLayer(slide_url_format, {
+        minZoom: 1,
+        maxZoom: 9
+    })
+
+    new L.Control.MiniMap(smallLayer, {
+        toggleDisplay: true,
+        position: 'bottomleft',
+        height: 200,
+        width: 200}).addTo(map)
 
 });
