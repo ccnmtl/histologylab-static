d376aa70058d03022b367e8140f079db5ef3d4e7
ndittren <36773036+ndittren@users.noreply.github.com>
2021-04-14 09:25:05 -0400

    Added specific magnification labels for all cases

--- themes/ctl-histologylab/static/js/src/tiles.js
+++ themes/ctl-histologylab/static/js/src/tiles.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
     var slideId = $('#map').data('slide-id');
     var zoomMax = $('#map').data('zoom');
-    var slide_url_format = 'http://localhost:8000/' + '/{z}/{y}/{x}.jpg';
+    var slide_url_format = 'https://ctl-webslides-static-prod.s3.amazonaws.com/slide' + slideId + '/{z}/{y}/{x}.jpg';
 
     var attribution = '&copy; Columbia University Department of Pathology and Cell Biology'
 
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
 		var text = L.DomUtil.create('div');
 		text.id = "info_text";
-		text.innerHTML = "Magnification: 40x"
+		text.innerHTML = "Magnification: 1x"
 		return text;
 		},
 
@@ -52,6 +52,171 @@ $(document).ready(function() {
             panControl: false,
             maxBounds: [[-90, -200], [90, 200]]
         }}).addTo(map)
-});
 
-    //$("#info_text")[0].innerHTML = 'new text'
+    map.on('zoomend', function () {
+        var zoomLevel = map.getZoom();
+        var mag = $("#info_text")[0];
+        if (slideId === 12 || slideId === 82) {
+            if (zoomMax === 8) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 25x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 40x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 50x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 60x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 75x'
+                        break;
+                    case 7:
+                        mag.innerHTML = 'Magnification: 85x'
+                        break;
+                    case 8:
+                        mag.innerHTML = 'Magnification: 100x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 25x'
+                }
+            }
+            else if (zoomMax === 9) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 10x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 20x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 35x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 45x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 60x'
+                        break;
+                    case 7:
+                        mag.innerHTML = 'Magnification: 75x'
+                        break;
+                    case 8:
+                        mag.innerHTML = 'Magnification: 90x'
+                        break;
+                    case 9:
+                        mag.innerHTML = 'Magnification: 100x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 10x'
+                }
+            }
+        } else {
+            if (zoomMax === 6) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 1x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 8x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 10x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 20x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 40x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 1x'
+                }
+            }
+            else if (zoomMax === 7) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 1x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 4x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 8x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 10x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 20x'
+                        break;
+                    case 7:
+                        mag.innerHTML = 'Magnification: 40x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 1x'
+                }
+            }
+            else if (zoomMax === 8) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 10x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 15x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 20x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 25x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 30x'
+                        break;
+                    case 7:
+                        mag.innerHTML = 'Magnification: 35x'
+                        break;
+                    case 8:
+                        mag.innerHTML = 'Magnification: 40x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 1x'
+                }
+            }
+            else if (zoomMax === 9) {
+                switch (zoomLevel) {
+                    case 2:
+                        mag.innerHTML = 'Magnification: 9x'
+                        break;
+                    case 3:
+                        mag.innerHTML = 'Magnification: 14x'
+                        break;
+                    case 4:
+                        mag.innerHTML = 'Magnification: 18x'
+                        break;
+                    case 5:
+                        mag.innerHTML = 'Magnification: 23x'
+                        break;
+                    case 6:
+                        mag.innerHTML = 'Magnification: 27x'
+                        break;
+                    case 7:
+                        mag.innerHTML = 'Magnification: 32x'
+                        break;
+                    case 8:
+                        mag.innerHTML = 'Magnification: 36x'
+                        break;
+                    case 9:
+                        mag.innerHTML = 'Magnification: 40x'
+                        break;
+                    default:
+                        mag.innerHTML = 'Magnification: 1x'
+                }
+            }
+        }
+    })
+});
